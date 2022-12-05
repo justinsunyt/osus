42d3ce5441bb057b215c6420658e63a802171a51
Justin Sun <justinsunyt@gmail.com>
2022-12-05 00:52:26 -0500

    Initial commit

---
+++ src/main/java/org/cis1200/Game.java
@@ -0,0 +1,22 @@
+package org.cis1200;
+
+import javax.swing.*;
+
+public class Game {
+    public record Employee(int id, String firstName,
+            String lastName) {
+    }
+
+    /**
+     * Main method run to start and run the game. Initializes the runnable game
+     * class of your choosing and runs it. IMPORTANT: Do NOT delete! You MUST
+     * include a main method in your final submission.
+     */
+    public static void main(String[] args) {
+        // Set the game you want to run here
+        Runnable game = new org.cis1200.mushroom.RunMushroomOfDoom();
+        Employee e1 = new Employee(1001, "Derok", "Dranf");
+
+        SwingUtilities.invokeLater(game);
+    }
+}
